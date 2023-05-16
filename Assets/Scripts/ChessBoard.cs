0006d0b4fa0176243ebb85ccc156205cf3907a15
navintc <navin.thamindu@gmail.com>
2023-05-16 17:55:24 +0530

    Adding the bases

---
+++ Assets/Scripts/ChessBoard.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChessBoard : MonoBehaviour
+{
+
+    public GameObject A1;
+    public GameObject A2;
+    public GameObject A3;
+    public GameObject A4;
+    public GameObject A5;
+    public GameObject A6;
+    public GameObject A7;
+    public GameObject A8;
+    public GameObject B1;
+    public GameObject B2;
+    public GameObject B3;
+    public GameObject B4;
+    public GameObject B5;
+    public GameObject B6;
+    public GameObject B7;
+    public GameObject B8;
+    public GameObject C1;
+    public GameObject C2;
+    public GameObject C3;
+    public GameObject C4;
+    public GameObject C5;
+    public GameObject C6;
+    public GameObject C7;
+    public GameObject C8;
+    public GameObject D1;
+    public GameObject D2;
+    public GameObject D3;
+    public GameObject D4;
+    public GameObject D5;
+    public GameObject D6;
+    public GameObject D7;
+    public GameObject D8;
+    public GameObject E1;
+    public GameObject E2;
+    public GameObject E3;
+    public GameObject E4;
+    public GameObject E5;
+    public GameObject E6;
+    public GameObject E7;
+    public GameObject E8;
+    public GameObject F1;
+    public GameObject F2;
+    public GameObject F3;
+    public GameObject F4;
+    public GameObject F5;
+    public GameObject F6;
+    public GameObject F7;
+    public GameObject F8;
+    public GameObject G1;
+    public GameObject G2;
+    public GameObject G3;
+    public GameObject G4;
+    public GameObject G5;
+    public GameObject G6;
+    public GameObject G7;
+    public GameObject G8;
+    public GameObject H1;
+    public GameObject H2;
+    public GameObject H3;
+    public GameObject H4;
+    public GameObject H5;
+    public GameObject H6;
+    public GameObject H7;
+    public GameObject H8;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
